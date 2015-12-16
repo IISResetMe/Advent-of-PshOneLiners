eec1938def32f0a13a8239dd8186a9ad0c5ea13e
IISResetMe <IISResetMe@users.noreply.github.com>
2015-12-17 00:18:10 +0100

    Advent of Code Day2
    http://adventofcode.com/day/2
    PowerShell

---
+++ Day2/2-1.ps1
@@ -0,0 +1,1 @@
+﻿param([string[]]$boxes)$boxes|%{$p=0}{$l,$w,$h=$_-split'x'-as[int[]];$p+=($l*$w+$w*$h+$h*$l)*2+(($l*$w*$h)/[Math]::Max($h,[Math]::Max($l,$w)))}{$p}
---
+++ Day2/2-2.ps1
@@ -0,0 +1,1 @@
+﻿param([string[]]$boxes)$boxes|%{$p=0}{$l,$w,$h=$_-split'x'-as[int[]];$p+=$l*$w*$h+($l+$w+$h-[Math]::Max($h,[Math]::Max($l,$w)))*2}{$p}
